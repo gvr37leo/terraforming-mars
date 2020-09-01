e9a276395d9d431c5f5d3294822a202fbd1de97f
paul boon <paul.boon@perplex.nl>
2020-09-01 14:20:35 +0200

    p

--- main.ts
+++ main.ts
@@ -34,10 +34,10 @@ class Player{
     terraformingpoints:number
 }
 
-var tags = ['science','microbe','earth','jupiter','electricity','titanium','event']
+var tags = ['science','microbe','animal','earth','jupiter','electricity','titanium','event','city','plant']
 
 class Card{
-    rules//oxygen/heat min/max restriction (maybe make table for rules with id's and description to make rendering easier)
+    rules//oxygen/heat/water min/max restriction (maybe make table for rules with id's and description to make rendering easier)
     id
     cost
     victorypoints
@@ -53,11 +53,29 @@ class Card{
     immediateEffect// like gain 25 metal (how to render on card// or do something with html templating and text description, ~rte with text,images and magic strings~)
 }
 
+class TileSlot{
+    tile:Tile
+    type = ['ocean','ground']
+    // resources
+    cards
+    metal
+    plants
+    titanium
+
+    // reserved
+    phobos
+    noctis
+    ganymede
+    
+}
+
 class Tile{
     owner
     sea
     city
     forest
+    
+
 }
 
 class Game{
